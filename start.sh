c36668652ce65453ed0ab2140ff0f004f73d9650
cute_panda <asdf3601a@gmail.com>
2023-05-31 23:07:04 +0800

    First work ver

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $KEY ]
+    then
+        echo -n "${KEY}" > /hath/data/client_login
+    else
+        if [ ! -f /hath/data/client_login ];
+            then
+                echo "Couldn't find the key."
+                exit 1
+        fi
+    
+fi
+
+./hath-rust
